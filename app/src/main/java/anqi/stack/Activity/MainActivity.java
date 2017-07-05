ba56c3c11bcacaea4b09220f14cd15389564421b
niuanqi <1542962286@qq.com>
2017-07-05 15:00:15 +0800

    异步post成功，异步get百度测试链接成功,同步的不成功

--- app/src/main/java/anqi/stack/Activity/MainActivity.java
+++ app/src/main/java/anqi/stack/Activity/MainActivity.java
@@ -1,39 +1,27 @@
 package anqi.stack.Activity;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import anqi.stack.R;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import anqi.stack.util.NetUtil;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final OkHttpClient client = new OkHttpClient();
+//    private final OkHttpClient client = new OkHttpClient();
     private TextView text;
     private final String basePath = "http://9.112.239.137:8080/p4-web-pg-debug";
-    private final String passWord = "p4admin" + "1qa2ws3ed";
-    private Handler h;
 
-    private String token=null;
+    private String token;
 
 
     @Override
@@ -41,79 +29,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.text);
-         h = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message msg) {
-                text.setText((String)msg.obj);
-                return false;
-            }
-        });
     }
 
     public void click(View view) {
-        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        JSONObject object = new JSONObject();
+
+        Map<String,String> params = new HashMap<>();
+        params.put("username","p4admin");
+        params.put("password","aq1sw2de");
+        JSONObject result = NetUtil.doPostAsync(basePath+"/api/auth/login",params);
         try {
-            object.put("username", "p4admin");
-            object.put("password", "aq1sw2de");
+            text.setText(result.getString("token"));
+            token = result.getString("token");
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        RequestBody body = RequestBody.create(JSON, String.valueOf(object));
 //        final Request request = new Request.Builder().url("http://api.map.baidu.com/location/ip?ak=请输入您的AK&coor=bd09ll").build();
-        Request request = new Request.Builder().url(basePath + "/api/auth/login").post(body).build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-                Log.d("tttt", "失败");
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-//                String sss = new String(res_ult.body().bytes());
-//                Log.d("json",sss);
-                String json = response.body().string();
-                try {
-                    JSONObject jsonObject = new JSONObject(json);
-                    String s = (String) jsonObject.get("token");
-                    token = s;
-                    Log.d("success", s);
-                    Message msg = Message.obtain();
-                    msg.obj = s;
-                    h.sendMessage(msg);
-
-                } catch (JsonSyntaxException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        });
     }
 
     public void click_json(View view) {
 
-        Gson json = new Gson();
-        String ss = json.fromJson("hsoghoang", String.class);
-        String you = json.toJson(false);
+//        Gson json = new Gson();
+//        String ss = json.fromJson("hsoghoang", String.class);
+//        String you = json.toJson(false);
+//
+//        Log.d("json", ss + you);
 
-        Log.d("json", ss + you);
+
     }
 
-//    public void get(View view){
-//        String url = basePath+"/app/nls/ZH_CN.json";
-//        Map params = new HashMap();
-//        params.put("token",token);
-//        JSONObject res = NetUtil.doGetSync(url,params);
-//
-//        try {
-//            Log.d("get test",res.getString("p4_title"));
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public void get(View view){
+        String test = "http://api.map.baidu.com/location/ip?ak=请输入您的AK&coor=bd09ll";
+        String url = basePath+"/app/nls/ZH_CN.json";
+        Map params = new HashMap();
+        params.put("token",token);
+        JSONObject res = NetUtil.doGetSync(test,params);
+
+        try {
+            Log.d("get test",res.getString("message"));
+            text.setText(res.getString("message"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
--- app/src/main/java/anqi/stack/util/NetUtil.java
+++ app/src/main/java/anqi/stack/util/NetUtil.java
@@ -1,15 +1,23 @@
 package anqi.stack.util;
 
+import android.util.Log;
+
+import com.google.gson.JsonSyntaxException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.Set;
 import java.util.concurrent.locks.ReentrantLock;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -18,7 +26,7 @@ import okhttp3.Response;
  */
 
 public class NetUtil {
-    private static OkHttpClient client;
+    private static OkHttpClient client  = new OkHttpClient();
     private static OkHttpClient.Builder builder;
     private static ReentrantLock lock = new ReentrantLock();
     private static long ConnecTimeOut = 10;//10秒链接超时
@@ -30,23 +38,21 @@ public class NetUtil {
     /**
      * 初始化Client,设置相应超时参数
      */
-    private static void initClient() {
-
-
-        if (builder == null) {
-            lock.lock();
-            builder = new OkHttpClient.Builder();
-            lock.unlock();
-        }
-        if (client == null) {
-            lock.lock();
-            client = builder.connectTimeout(ConnecTimeOut, TimeUnit.SECONDS)
-                    .writeTimeout(WriteTimeOut, TimeUnit.SECONDS)
-                    .readTimeout(ReadTimeOut, TimeUnit.SECONDS)
-                    .build();
-            lock.unlock();
-        }
-    }
+//     static  {
+//        if (builder == null) {
+//            lock.lock();
+//            builder = new OkHttpClient.Builder();
+//            lock.unlock();
+//        }
+//        if (client == null) {
+//            lock.lock();
+//            client = builder.connectTimeout(ConnecTimeOut, TimeUnit.SECONDS)
+//                    .writeTimeout(WriteTimeOut, TimeUnit.SECONDS)
+//                    .readTimeout(ReadTimeOut, TimeUnit.SECONDS)
+//                    .build();
+//            lock.unlock();
+//        }
+//    }
 
     /**
      * 同步get请求
@@ -55,18 +61,18 @@ public class NetUtil {
      * @return 结果json
      */
     public static JSONObject doGetSync(String url, Map params) {
-        initClient();
 
         final Request request = new Request.Builder()
                 .url(url)
-                .header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:52.0) Gecko/20100101 Firefox/52.0")
-                .addHeader("Accept", "application/json, text/plain, */*")
-                .addHeader("Accept-Encoding","gzip, deflate")
-                .addHeader("Accept-Language","zh-CN,en-US;q=0.7,en;q=0.3")
-                .addHeader("Host","9.112.239.179:8080")
-                .addHeader("Referer","http://9.112.239.179:8080/p4-web-pg-debug/")
-                .addHeader("IPC_TOKEN", (String) params.get("token"))
+//                .header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:52.0) Gecko/20100101 Firefox/52.0")
+//                .addHeader("Accept", "application/json, text/plain, */*")
+//                .addHeader("Accept-Encoding","gzip, deflate")
+//                .addHeader("Accept-Language","zh-CN,en-US;q=0.7,en;q=0.3")
+//                .addHeader("Host","9.112.239.179:8080")
+//                .addHeader("Referer","http://9.112.239.179:8080/p4-web-pg-debug/")
+//                .addHeader("IPC_TOKEN", (String) params.get("token"))
                 .build();
+
         try {
             Response response = client.newCall(request)
                     .execute();
@@ -83,12 +89,35 @@ public class NetUtil {
     /**
      * 同步post请求
      *
-     * @param rul
+     * @param url
      * @param params
      * @return
      */
-    public static JSONObject doPostSyc(String rul, Map params) {
-        initClient();
+    public static JSONObject doPostSyc(String url, Map params) {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        JSONObject object = new JSONObject();
+        try {
+            Set<String> keySet = params.keySet();
+            for (String key:keySet) {
+                object.put(key,params.get(key));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(JSON, String.valueOf(object));
+        final  Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String Result = response.body().string();
+            resJson = new JSONObject(Result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return resJson;
     }
 
@@ -99,7 +128,34 @@ public class NetUtil {
      * @return
      */
     public static JSONObject doGetAsync(String url, Map params) {
-        initClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:52.0) Gecko/20100101 Firefox/52.0")
+                .addHeader("Accept", "application/json, text/plain, */*")
+                .addHeader("Accept-Encoding","gzip, deflate")
+                .addHeader("Accept-Language","zh-CN,en-US;q=0.7,en;q=0.3")
+                .addHeader("Host","9.112.239.179:8080")
+                .addHeader("Referer","http://9.112.239.179:8080/p4-web-pg-debug/")
+                .addHeader("IPC_TOKEN", (String) params.get("token"))
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d("失败","failed");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String res = response.body().string();
+                try {
+                    resJson = new JSONObject(res);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        while (resJson ==null) Log.d("get等待","wait");
         return resJson;
     }
 
@@ -111,8 +167,43 @@ public class NetUtil {
      * @return
      */
     public static JSONObject doPostAsync(String url, Map params) {
-        initClient();
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        JSONObject object = new JSONObject();
+        try {
+            Set<String> keySet = params.keySet();
+            for (String key:keySet) {
+                object.put(key,params.get(key));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(JSON, String.valueOf(object));
+        Request request = new Request.Builder().url(url).post(body).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("failed", "failed");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                try {
+                    resJson = new JSONObject(json);
+                    String s = (String) resJson.get("token");
+                    Log.d("success", s);
+
+                } catch (JsonSyntaxException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        while (resJson ==null) Log.d("等待","wait");
         return resJson;
     }
+
 
 }
